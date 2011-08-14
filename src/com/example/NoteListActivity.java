a52bf19d75bcfd56edb90575d17e274039c7f1c1
SATYA <satya@ubuntu.ubuntu-domain>
2011-08-14 17:09:02 +0530

    add note functionality added

--- src/com/example/NoteListActivity.java
+++ src/com/example/NoteListActivity.java
@@ -2,7 +2,9 @@ package com.example;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import com.orm.SugarRecord;
 
@@ -15,34 +17,16 @@ public class NoteListActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notelist);
-        initDb();
 
-        List<Note> notes = getNotes();
+        List<Note> notes = Note.listAll(this, Note.class);
         setListAdapter(new ArrayAdapter<Note>(this,android.R.layout.simple_list_item_1, notes));
 
+        findViewById(R.id.Button01).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(NoteListActivity.this, AddNoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private List<Note> getNotes() {
-        return Note.listAll(this, Note.class);
-       // return list();
-    }
-
-    private void initDb() {
-
-        Tag t1 = new Tag(this, "tag1");
-        Tag t2 = new Tag(this, "tag2");
-        t1.save();
-        t2.save();
-
-        Note n1 = new Note(this, "note1", "description1", t1);
-        Note n2 = new Note(this, "note2", "description2", t1);
-        Note n3 = new Note(this, "note3", "description3", t2);
-        Note n4 = new Note(this, "note4", "description4", t2);
-
-        n1.save();
-        n2.save();
-        n3.save();
-        n4.save();
-
-    }
 }
--- src/com/example/SugarActivity.java
+++ src/com/example/SugarActivity.java
@@ -12,7 +12,29 @@ public class SugarActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+         Note.deleteAll(this, Note.class);
+        Tag.deleteAll(this, Tag.class);
+        initDb();
         Intent intent = new Intent(this, NoteListActivity.class);
         startActivity(intent);
+    }
+
+     private void initDb() {
+
+        Tag t1 = new Tag(this, "tag1");
+        Tag t2 = new Tag(this, "tag2");
+        t1.save();
+        t2.save();
+
+        Note n1 = new Note(this, "note1", "description1", t1);
+        Note n2 = new Note(this, "note2", "description2", t1);
+        Note n3 = new Note(this, "note3", "description3", t2);
+        Note n4 = new Note(this, "note4", "description4", t2);
+
+        n1.save();
+        n2.save();
+        n3.save();
+        n4.save();
+
     }
 }
